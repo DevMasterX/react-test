ef8b6a5f91171d7895fc1f1c03098e644967e78b
DevMasterX <sokol080288@gmail.com>
2023-09-02 01:28:41 +0300

    card

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const root = document.getElementById("root");
 //   children: ["I am P"],
 // });
 
-const p = <p id="test-ID">I am P</p>;
+// const p = <p id="test-ID">I am P</p>;
 
 // const div = React.createElement("div", {
 //   name: "main",
@@ -22,14 +22,31 @@ const p = <p id="test-ID">I am P</p>;
 //   children: p,
 // });
 
-const div = (
-  <div name="main" id="test" className="test-class">
-    {p}
+// const div = (
+//   <div name="main" id="test" className="test-class">
+//     {p}
+//   </div>
+// );
+
+// console.log(div);
+
+const card = (
+  <div class="card" style={{ width: "18rem" }}>
+    <img src={data[0].url} class="card-img-top" alt={data[0].title} />
+    <div class="card-body">
+      <h5 class="card-title">{data[0].title}</h5>
+      <p class="card-text">
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </p>
+      <a href="http" class="btn btn-primary">
+        Go somewhere
+      </a>
+    </div>
   </div>
 );
 
-console.log(div);
-ReactDOM.createRoot(root).render(div);
+ReactDOM.createRoot(root).render(card);
 
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(<React.StrictMode></React.StrictMode>);
