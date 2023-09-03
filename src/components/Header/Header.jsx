0cb8977836a2715add226de4b38c3d9ae07415ec
DevMasterX <sokol080288@gmail.com>
2023-09-04 00:47:16 +0300

    1111

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,35 +1,12 @@
-import { styled } from "styled-components";
-
-// -------1 option
-
-// const TypographyHeader = styled.h1`
-//   color: #510d24;
-//   margin: 0;
-//   padding: ${({ isOnline }) => (isOnline ? 130 : 12)};
-// `;
-
-// ------2 option
-
-// const color = "#000";
-// const TypographyHeader = styled("h1")({
-//   color: "aqua",
-//   boxShadow: ({ isOnline }) => `0 0 12px 12px ${isOnline ? color : "#00fbff"}}`,
-// });
-
-// ------3 option - the best
-
-const TypographyHeader = styled("h1")(({ isOnline }) => {
-  return {
-    color: isOnline ? "black" : "red",
-    textAlign: isOnline ? "center" : "right",
-  };
-});
+// import { styled } from "styled-components";
+import { TypographyHeader, TypographyText } from "./Header.styled";
 
 const Header = () => {
   const isOnline = true;
   return (
     <div>
       <TypographyHeader $isOnline={isOnline}>Hello React</TypographyHeader>
+      <TypographyText>qwerty</TypographyText>
     </div>
   );
 };
