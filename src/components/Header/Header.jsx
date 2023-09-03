f4e96e46f27452d8eb9e2f11b448c8eb7119e2ea
DevMasterX <sokol080288@gmail.com>
2023-09-04 00:20:20 +0300

    styled components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,29 @@
 import { styled } from "styled-components";
 
-const TypographyHeader = styled.h1`
-  color: #510d24;
-  margin: 0;
-  padding: ${({ isOnline }) => (isOnline ? 130 : 12)};
-`;
+// -------1 option
+
+// const TypographyHeader = styled.h1`
+//   color: #510d24;
+//   margin: 0;
+//   padding: ${({ isOnline }) => (isOnline ? 130 : 12)};
+// `;
+
+// ------2 option
+
+// const color = "#000";
+// const TypographyHeader = styled("h1")({
+//   color: "aqua",
+//   boxShadow: ({ isOnline }) => `0 0 12px 12px ${isOnline ? color : "#00fbff"}}`,
+// });
+
+// ------3 option - the best
+
+const TypographyHeader = styled("h1")(({ isOnline }) => {
+  return {
+    color: isOnline ? "black" : "red",
+    textAlign: isOnline ? "center" : "right",
+  };
+});
 
 const Header = () => {
   const isOnline = true;
