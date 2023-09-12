409fec97f3447042a76a927424ca09482b750c58
DevMasterX <sokol080288@gmail.com>
2023-09-13 01:03:50 +0300

    Dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import Header from "./components/Header/Header";
-import Content from "./components/Content";
-import Counter from "./components/Counter/Counter";
+// import Header from "./components/Header/Header";
+// import Content from "./components/Content";
+// import Counter from "./components/Counter/Counter";
+import Dropdown from "./components/Dropdown/Dropdown";
 
 const App = () => {
   return (
     <>
-      <Header />
+      {/* <Header />
       <Content />
       <h1>Состояние компонента</h1>
-      <Counter initialValue={10} />
+      <Counter initialValue={10} /> */}
+      <Dropdown />
     </>
   );
 };
