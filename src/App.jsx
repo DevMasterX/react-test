d4db1a8cf1166619fe1b5494fb582aee186ca7cb
DevMasterX <sokol080288@gmail.com>
2023-09-14 00:18:00 +0300

    todoList

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,59 @@
 // import Header from "./components/Header/Header";
 // import Content from "./components/Content";
 // import Counter from "./components/Counter/Counter";
-import ColorPicker from "./components/ColorPicker/ColorPicker";
-import Dropdown from "./components/Dropdown/Dropdown";
+import { Component } from "react";
+// import ColorPicker from "./components/ColorPicker";
+// import Dropdown from "./components/Dropdown/Dropdown";
+import TodoList from "./components/TodoList";
+import initialTodos from "./todos.json";
 
-const colorPickerOptions = [
-  { label: "red", color: "#f44336" },
-  { label: "green", color: "#00fd0d" },
-  { label: "blue", color: "#4202f0" },
-  { label: "grey", color: "#656565" },
-  { label: "pink", color: "#f90eca" },
-  { label: "indigo", color: "#3f51b5" },
-];
+// const colorPickerOptions = [
+//   { label: "red", color: "#f44336" },
+//   { label: "green", color: "#00fd0d" },
+//   { label: "blue", color: "#4202f0" },
+//   { label: "grey", color: "#656565" },
+//   { label: "pink", color: "#f90eca" },
+//   { label: "indigo", color: "#3f51b5" },
+// ];
 
-const App = () => {
-  return (
-    <>
-      {/* <Header />
+class App extends Component {
+  state = {
+    todos: initialTodos,
+  };
+
+  deleteTodo = (todoId) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.filter((todo) => todo.id !== todoId),
+    }));
+  };
+
+  render() {
+    const { todos } = this.state;
+
+    const totalTodoCount = todos.length;
+    const completedTodosCount = todos.reduce(
+      (total, todo) => (todo.completed ? total + 1 : total),
+      0
+    );
+
+    return (
+      <>
+        {/* <Header />
       <Content />
       <h1>Состояние компонента</h1>
       <Counter initialValue={10} /> */}
-      <Dropdown />
-      <ColorPicker options={colorPickerOptions} />
-    </>
-  );
-};
+        {/* <Dropdown /> */}
+        {/* <ColorPicker options={colorPickerOptions} /> */}
+
+        <div>
+          <p>Общее кол-во: {totalTodoCount}</p>
+          <p>Кол-во выполненных: {completedTodosCount}</p>
+        </div>
+
+        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
+      </>
+    );
+  }
+}
 
 export default App;
