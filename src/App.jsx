abb4339bfdb07cceba00eff505e3b15b854054af
DevMasterX <sokol080288@gmail.com>
2023-09-13 22:21:37 +0300

    colorPicker

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 // import Header from "./components/Header/Header";
 // import Content from "./components/Content";
 // import Counter from "./components/Counter/Counter";
+import ColorPicker from "./components/ColorPicker/ColorPicker";
 import Dropdown from "./components/Dropdown/Dropdown";
+
+const colorPickerOptions = [
+  { label: "red", color: "#f44336" },
+  { label: "green", color: "#00fd0d" },
+  { label: "blue", color: "#4202f0" },
+  { label: "grey", color: "#656565" },
+  { label: "pink", color: "#f90eca" },
+  { label: "indigo", color: "#3f51b5" },
+];
 
 const App = () => {
   return (
@@ -11,6 +21,7 @@ const App = () => {
       <h1>Состояние компонента</h1>
       <Counter initialValue={10} /> */}
       <Dropdown />
+      <ColorPicker options={colorPickerOptions} />
     </>
   );
 };
