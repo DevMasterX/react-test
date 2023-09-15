8b898c53fd7004df9667b650116045606c406cf5
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2023-09-15 16:10:56 +0300

    1231

--- src/components/Counter1/Counter1.jsx
+++ src/components/Counter1/Counter1.jsx
@@ -6,6 +6,15 @@ const Counter1 = () => {
       <div className="card bg-dark text-white" style={{ width: "600px" }}>
         <div className="card-body">
           <h5 className="card-title text-center fs-1">Counter</h5>
+          <p className='card-text text-center ' stle={{fontSize:'80px'}}>0</p>
+          <div className='d-flex justify-content-center px-5'>
+            <button className='btn btn-outline-success me-5'>
+              <i className='bi bi-plus-circle fs-1'></i>
+            </button>
+            <button className='btn btn-outline-danger ms-5'>
+              <i className='bi bi-dash-circle fs-1'></i>
+            </button>
+          </div>
         </div>
       </div>
     </div>
