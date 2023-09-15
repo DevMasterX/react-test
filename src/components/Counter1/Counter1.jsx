4270c2985c4d9b299d1f0c2fe9dd1366c1ef3165
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2023-09-15 16:15:49 +0300

    112211

--- src/components/Counter1/Counter1.jsx
+++ src/components/Counter1/Counter1.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
+let counter = 0;
+
 const Counter1 = () => {
   return (
     <div className="position-absolute top-50 start-50 translate-middle">
       <div className="card bg-dark text-white" style={{ width: "600px" }}>
         <div className="card-body">
           <h5 className="card-title text-center fs-1">Counter</h5>
-          <p className='card-text text-center ' stle={{fontSize:'80px'}}>0</p>
+          <p className='card-text text-center ' stle={{fontSize:'80px'}}>{counter}</p>
           <div className='d-flex justify-content-center px-5'>
             <button className='btn btn-outline-success me-5'>
               <i className='bi bi-plus-circle fs-1'></i>
