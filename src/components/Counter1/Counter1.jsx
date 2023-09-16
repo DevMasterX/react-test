3edb965c8060a91b4e809f51538bc460dcf6991a
DevMasterX <sokol080288@gmail.com>
2023-09-17 00:39:22 +0300

    Component1

--- src/components/Counter1/Counter1.jsx
+++ src/components/Counter1/Counter1.jsx
@@ -1,26 +1,71 @@
-import React from "react";
+import  { Component } from "react";
 
-let counter = 0;
 
-const Counter1 = () => {
-  return (
-    <div className="position-absolute top-50 start-50 translate-middle">
-      <div className="card bg-dark text-white" style={{ width: "600px" }}>
-        <div className="card-body">
-          <h5 className="card-title text-center fs-1">Counter</h5>
-          <p className='card-text text-center ' stle={{fontSize:'80px'}}>{counter}</p>
-          <div className='d-flex justify-content-center px-5'>
-            <button className='btn btn-outline-success me-5'>
-              <i className='bi bi-plus-circle fs-1'></i>
-            </button>
-            <button className='btn btn-outline-danger ms-5'>
-              <i className='bi bi-dash-circle fs-1'></i>
-            </button>
+class Counter1 extends Component {
+state={counter:0}
+
+handleClickIncrement =() => {
+  this.setState((prev)=>{
+    return {counter: prev.counter + 1}
+  });
+}
+
+handleClickDecrement =() => {
+  this.setState((prev)=>{
+    return {counter: prev.counter -1}
+  });
+}
+
+render(){
+return(
+      <div className="position-absolute top-50 start-50 translate-middle">
+        <div className="card bg-dark text-white" style={{ width: "600px" }}>
+          <div className="card-body">
+            <h5 className="card-title text-center fs-1">Counter</h5>
+            <p className='card-text text-center ' style={{ fontSize: '80px  !important'}} >{this.state.counter}</p>
+            <div className='d-flex justify-content-center px-5'>
+              <button onClick={this.handleClickIncrement} className='btn btn-outline-success me-5'>
+                <i className='bi bi-plus-circle fs-1'></i>
+              </button>
+              <button onClick={this.handleClickDecrement} className='btn btn-outline-danger ms-5'>
+                <i className='bi bi-dash-circle fs-1'></i>
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+
+}
+}
+
+
+
+
+// let counter = 0;
+
+// const Counter1 = () => {
+//   const handleClick = () => {
+    
+//   }
+//   return (
+//     <div className="position-absolute top-50 start-50 translate-middle">
+//       <div className="card bg-dark text-white" style={{ width: "600px" }}>
+//         <div className="card-body">
+//           <h5 className="card-title text-center fs-1">Counter</h5>
+//           <p className='card-text text-center ' style={{ fontSize: '80px  !important'}} >{counter}</p>
+//           <div className='d-flex justify-content-center px-5'>
+//             <button onClick={handleClick} className='btn btn-outline-success me-5'>
+//               <i className='bi bi-plus-circle fs-1'></i>
+//             </button>
+//             <button className='btn btn-outline-danger ms-5'>
+//               <i className='bi bi-dash-circle fs-1'></i>
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
 export default Counter1;
